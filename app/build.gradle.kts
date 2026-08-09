2470a7a61f5049b3e67960b1602c404c3725d99f
ravisankarg <ravisankar.g@gmail.com>
2026-08-09 20:45:46 +0530

    Release Ask Galaxy V0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.ravi.askgalaxy"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.6"
+        versionCode = 7
+        versionName = "0.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
